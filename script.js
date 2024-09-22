d2adb1318e6c650d80c70fa6bb2b0708fad6699c
lievflet <lievflet@gmail.com>
2024-09-22 13:01:28 -0700

    “update”

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ function toggleNav() {
       x.className = "topnav";
     }
   }
+
+  function handleNavigation(event) {
+    const clickedElement = event.target.closest('.clickable');
+    if (clickedElement) {
+        const href = clickedElement.getAttribute('data-href');
+        if (href) {
+            window.location.href = href;
+        }
+    }
+  }
+  
+  function handleCaseStudyButtonClick() {
+    window.location.href = '/school-ride.html';
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const bigContainer = document.getElementById('big-container');
+    if (bigContainer) {
+        bigContainer.addEventListener('click', handleNavigation);
+    }
+  
+    const caseStudyButton = document.getElementById('caseStudyButton');
+    if (caseStudyButton) {
+        caseStudyButton.addEventListener('click', handleCaseStudyButtonClick);
+    }
+  });
